62c0dfe6d397953a9f2195512a3f39b6e5dc52fa
hrushennka <alinauglova66@gmail.com>
2025-04-14 04:58:27 +0300

    Вывод статистики на страницу

--- frontend/scripts/api.js
+++ frontend/scripts/api.js
@@ -149,4 +149,13 @@ export default {
 
     return response.data;
   },
+
+  getStatistics: async (patientId, startDate, endDate) => {
+    console.log(patientId, startDate, endDate);
+    const response = await api.get(`/statistic/get/${patientId}`, {
+      params: { startDate, endDate },
+    });
+
+    return response.data;
+  },
 };
