dbc3f445298c1ae4dac4a057d53341325c67cc3a
Mooncat3 <mark.frizen730@gmail.com>
2025-04-17 18:58:02 +0300

    Разделение функций, представлений и таблиц по разным файлам

--- database/views/users_pub.sql
+++ database/views/users_pub.sql
@@ -1,21 +1,3 @@
--- Представление "Список пациентов для пользователя"
-CREATE VIEW patients_pub AS
-SELECT 
-    u.firstname, 
-    u.surname, 
-    u.lastname, 
-	u.login,
-    u.email, 
-    ur.patient_id,
-    ur.level,
-    ur.activity
-FROM userrelations ur
-JOIN users u ON ur.patient_id = u.user_id
-WHERE ur.doctor_id = current_setting('app.user_uuid')::UUID
-
-GRANT SELECT ON patients_pub TO backend
-
-
 --Представление "Информация для каждого пользователя"
 CREATE VIEW users_pub AS
 SELECT 
@@ -38,4 +20,3 @@ LEFT JOIN userrelations ur ON ur.patient_id = u.user_id
 WHERE u.user_id = current_setting('app.user_uuid')::UUID
 
 GRANT SELECT ON users_pub TO backend
-
